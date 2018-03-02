f4141aa37d9f760f10a044781c5657eb66c7c779
Gerry Mc Donnell <irishbloke@gmail.com>
2018-03-02 19:27:29 +0000

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    
+    <!--header here-->
+    <app-header></app-header>
+    
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+//import header component
+import Header from './components/Header.vue';
+
 export default {
- 
+  components:{
+    appHeader:Header
+  }
 }
+
 </script>
 
 <style>
