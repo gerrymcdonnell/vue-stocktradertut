df7bd8c666f729111607c6b3f27db2d062b903ad
Gerry Mc Donnell <irishbloke@gmail.com>
2018-03-02 19:52:53 +0000

    stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,70 @@
 <template>
-  <h1>stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    NAME
+                    <small>(Price: PRICE)</small>
+                </h3>
+            </div>
+
+            
+            <div class="panel-body">
+                
+                <div class="pull-left">
+                    <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity"
+                           
+                    >
+                </div>
+                
+                <div class="pull-right">
+                    <button
+                            class="btn btn-success"                            
+                    >Buy
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
+
+<script>
+    /*export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.$store.dispatch('buyStock', order);
+                this.quantity = 0;
+            }
+        }
+    }*/
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>stocks component</h1>
+    <div>
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
+
+<script>
+
+    /*import Stock from './Stock.vue';
+
+    export default {
+        components: {
+            appStock: Stock
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
+        }
+    }*/
+
+
+
+import Stock from './Stock.vue'
+
+export default {
+    data(){
+        return{
+            stocks:[
+                {id:1,name:'BMW',price:120},
+                {id:2,name:'BMW1',price:121},
+                {id:3,name:'BMW2',price:122}
+            ]
+        }
+    },
+    components:{
+        appStock:Stock
+    }
+}
+
+</script>
